60af1fed2b2a3f53d6267366edf66a8d7264282f
MosesAddai <maddaibaffour@gmail.com>
2024-07-06 08:58:20 +0000

    Make Tweaks on App.js file.
    BackgroudColor changed to Aqua blue.
    Text in Text Tag changed.
    Text in Text Tag styled (FontSize and FontWeight changed).

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>My Name is "Moses Addai-Baffour Owusu"</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#02D8E9',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  }
 });
